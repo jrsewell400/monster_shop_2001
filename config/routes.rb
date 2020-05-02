572230c7040232a7c588a7fdc1b54467a9ddf7a9
Jordan Sewell <jrsewell400@gmail.com>
2020-05-02 08:06:34 -0600

    Refactor remaining routes and add final touches

--- config/routes.rb
+++ config/routes.rb
@@ -2,33 +2,41 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   #welcome
-  get "/", to: "welcome#index"
+  root "welcome#index"
+  # get "/", to: "welcome#index"
 
   #merchants
-  get "/merchants", to: "merchants#index"
-  get "/merchants/new", to: "merchants#new"
-  get "/merchants/:id", to: "merchants#show"
-  post "/merchants", to: "merchants#create"
-  get "/merchants/:id/edit", to: "merchants#edit"
-  patch "/merchants/:id", to: "merchants#update"
-  delete "/merchants/:id", to: "merchants#destroy"
+  resources :merchants do 
+    resources :items, only: [:index, :new, :create]
+  end
+  # get "/merchants", to: "merchants#index"
+  # get "/merchants/new", to: "merchants#new"
+  # get "/merchants/:id", to: "merchants#show"
+  # post "/merchants", to: "merchants#create"
+  # get "/merchants/:id/edit", to: "merchants#edit"
+  # patch "/merchants/:id", to: "merchants#update"
+  # delete "/merchants/:id", to: "merchants#destroy"
 
   #items
-  get "/items", to: "items#index"
-  get "/items/:id", to: "items#show"
-  get "/items/:id/edit", to: "items#edit"
-  patch "/items/:id", to: "items#update"
-  get "/merchants/:merchant_id/items", to: "items#index"
-  get "/merchants/:merchant_id/items/new", to: "items#new"
-  post "/merchants/:merchant_id/items", to: "items#create"
-  delete "/items/:id", to: "items#destroy"
+  resources :items, only: [:index, :show, :edit, :update, :destroy] do 
+    resources :reviews, only: [:new, :create]
+  end
+  # get "/items", to: "items#index"
+  # get "/items/:id", to: "items#show"
+  # get "/items/:id/edit", to: "items#edit"
+  # patch "/items/:id", to: "items#update"
+  # delete "/items/:id", to: "items#destroy"
+  # get "/merchants/:merchant_id/items", to: "items#index"
+  # get "/merchants/:merchant_id/items/new", to: "items#new"
+  # post "/merchants/:merchant_id/items", to: "items#create"
 
   #reviews
-  get "/items/:item_id/reviews/new", to: "reviews#new"
-  post "/items/:item_id/reviews", to: "reviews#create"
-  get "/reviews/:id/edit", to: "reviews#edit"
-  patch "/reviews/:id", to: "reviews#update"
-  delete "/reviews/:id", to: "reviews#destroy"
+  resources :reviews, only: [:edit, :update, :destroy]
+  # get "/reviews/:id/edit", to: "reviews#edit"
+  # patch "/reviews/:id", to: "reviews#update"d
+  # delete "/reviews/:id", to: "reviews#destroy"
+  # get "/items/:item_id/reviews/new", to: "reviews#new"
+  # post "/items/:item_id/reviews", to: "reviews#create"
 
   #cart
   post "/cart/:item_id", to: "cart#add_item"
@@ -39,9 +47,10 @@ Rails.application.routes.draw do
   delete "/cart/:item_id", to: "cart#remove_item"
 
   #orders
-  get "/orders/new", to: "orders#new"
-  post "/orders", to: "orders#create"
-  get "/orders/:id", to: "orders#show"
+  resources :orders, only: [:new, :create, :show]
+  # get "/orders/new", to: "orders#new"
+  # post "/orders", to: "orders#create"
+  # get "/orders/:id", to: "orders#show"
 
   #sessions
   get "/login", to: "sessions#new"
@@ -63,35 +72,38 @@ Rails.application.routes.draw do
   #merchant
   namespace :merchant do
     get '/dashboard', to: 'dashboard#show'
-    get '/items', to: 'items#index'
-    patch '/items/:id', to: 'items#update'
-    delete '/items/:id', to: 'items#destroy'
-    get '/items/new', to: 'items#new'
-    post '/items', to: 'items#create'
-    get 'items/:id/edit', to: 'items#edit'
-    get '/orders/:id', to: 'orders#show'
-    patch '/item_orders/:id/', to: 'item_orders#update'
-
+    resources :items, only: [:index, :new, :create, :edit, :update, :destroy]
+    # get '/items', to: 'items#index'
+    # patch '/items/:id', to: 'items#update'
+    # delete '/items/:id', to: 'items#destroy'
+    # get '/items/new', to: 'items#new'
+    # post '/items', to: 'items#create'
+    # get 'items/:id/edit', to: 'items#edit'
+    resources :orders, only: [:show]
+    # get '/orders/:id', to: 'orders#show'
+    resources :item_orders, only: [:update]
+    # patch '/item_orders/:id', to: 'item_orders#update'
   end
 
   #admin
   namespace :admin do
     get "/dashboard", to: "dashboard#show"
-    get '/users', to: 'users#index'
-    get "/users/:user_id", to: "users#show"
-    get '/users/:user_id/password/edit', to: 'users_password#edit'
-    patch '/users/:user_id/password/edit', to: 'users_password#update'
-    get '/users/:user_id/edit', to: 'users#edit'
-    patch '/users/:user_id', to: 'users#update'
-    get '/users/:user_id/orders', to: 'users_orders#index'
-    get '/merchants', to: 'merchants#index'
+    resources :users, only: [:index, :show, :edit, :update] do 
+      get 'password/edit', to: 'users_password#edit'
+      patch '/password/edit', to: 'users_password#update'
+      get '/orders', to: 'users_orders#index'
+    end
+    # get '/users', to: 'users#index'
+    # get "/users/:user_id", to: "users#show"
+    # get '/users/:user_id/edit', to: 'users#edit'
+    # patch '/users/:user_id', to: 'users#update'
     resources :items
     resources :orders, only: [:update]
     resources :merchants do
-      get '/items', to: 'merchant_items#index'
-      get '/items/:item_id/edit', to: 'merchant_items#edit'
-      patch '/items/:item_id', to: 'merchant_items#update'
-  
+      resources :items, only: [:index, :edit, :update], controller: 'merchant_items'
+      # get '/items', to: 'merchant_items#index'
+      # get '/items/:item_id/edit', to: 'merchant_items#edit'
+      # patch '/items/:item_id', to: 'merchant_items#update'
     end
   end
 
